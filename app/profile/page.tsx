1d28b470b9a35489f03a359aa45d1780930748e7
4mser <nicolas.moreno.sagan@gmail.com>
2024-05-30 03:14:40 -0400

    test reproductor react

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import ProtectedRoute from '@/components/ProtectedRoute';
 import LogoutButton from '@/components/LogoutButton';
@@ -9,6 +9,7 @@ import VideoModal from '@/components/VideoModal';
 import api from '@/services/api';
 import { useRouter } from 'next/navigation';
 import { UserTrick } from '@/types/usertrick';
+import ReactPlayer from 'react-player';
 
 const useUnlockedTricks = (userId: string | undefined) => {
   const [unlockedTricks, setUnlockedTricks] = useState<UserTrick[]>([]);
@@ -87,21 +88,21 @@ const UserProfile: React.FC = () => {
   if (!user) return <div>No user data</div>;
 
   return (
-    <div className="bg-neutral-950 text-white shadow-lg  pb-32 relative">
+    <div className="bg-neutral-950 text-white shadow-lg pb-32 relative">
       {isEditing ? (
         <EditProfileForm onClose={() => setIsEditing(false)} onProfileUpdate={handleProfileUpdate} />
       ) : (
         <>
-          <div className='relative p-5 bg-gradient-to-t from-neutral-900 to-transparent m-5 rounded-3xl'>
-            <div className='relative z-10 flex flex-col items-center text-center gap-2'>
+          <div className="relative p-5 bg-gradient-to-t from-neutral-900 to-transparent m-5 rounded-3xl">
+            <div className="relative z-10 flex flex-col items-center text-center gap-2">
               <img
                 src={user.profilePictureUrl || '/profile.jpeg'}
                 alt="Profile Picture"
                 className="w-24 h-24 object-cover rounded-3xl shadow-2xl shadow-white/30"
               />
               <h2 className="text-2xl font-bold">{user.username}</h2>
-              <p className='px-5 opacity-70 font-light'>{user.description}</p>
-              <div className='flex gap-3'>
+              <p className="px-5 opacity-70 font-light">{user.description}</p>
+              <div className="flex gap-3">
                 <button
                   onClick={() => setIsEditing(true)}
                   className="bg-neutral-800 border border-px border-white/0 hover:border-white/10 hover:bg-neutral-900 text-white p-2 rounded-md text-xs"
@@ -111,24 +112,24 @@ const UserProfile: React.FC = () => {
                 <LogoutButton />
               </div>
             </div>
-            <div className='m-6 space-y-2'>
-              <p className='text-white'>Progreso trucos: {progressPercentage}%</p>
-              <div className='w-full h-2 bg-neutral-700 rounded-full'>
-                <div className='h-full bg-green-400 rounded-full' style={{ width: `${progressPercentage}%` }} />
+            <div className="m-6 space-y-2">
+              <p className="text-white">Progreso trucos: {progressPercentage}%</p>
+              <div className="w-full h-2 bg-neutral-700 rounded-full">
+                <div className="h-full bg-green-400 rounded-full" style={{ width: `${progressPercentage}%` }} />
               </div>
             </div>
-            <ul className='relative z-10 bg-neutral-950 mx-5 py-2 flex justify-around rounded-full '>
-              <li className='flex flex-col items-center'>
+            <ul className="relative z-10 bg-neutral-950 mx-5 py-2 flex justify-around rounded-full">
+              <li className="flex flex-col items-center">
                 <p>{user.level}</p>
-                <p className='text-xs opacity-50'>Nivel</p>
+                <p className="text-xs opacity-50">Nivel</p>
               </li>
-              <li className='flex flex-col items-center'>
+              <li className="flex flex-col items-center">
                 <p>{user.experiencePoints}</p>
-                <p className='text-xs opacity-50'>XP</p>
+                <p className="text-xs opacity-50">XP</p>
               </li>
-              <li className='flex flex-col items-center'>
+              <li className="flex flex-col items-center">
                 <p>{user.skateCoinsBalance}</p>
-                <p className='text-xs opacity-50'>SC</p>
+                <p className="text-xs opacity-50">SC</p>
               </li>
             </ul>
           </div>
@@ -145,17 +146,20 @@ const UserProfile: React.FC = () => {
                     >
                       {trick.trickId.name}
                     </div>
-                    <video
+                    <ReactPlayer
+                      url={trick.videoUrl}
+                      playing={false}
+                      controls={true}
+                      width="100%"
+                      height="100%"
                       onClick={() => setSelectedTrick(trick)}
-                      className="w-full h-full object-cover cursor-pointer"
-                      src={`${trick.videoUrl}`}
-                      muted
+                      className="cursor-pointer"
                     />
                   </div>
                 ))}
               </div>
             ) : (
-              <p className='px-5'>Sin trucos aún.</p>
+              <p className="px-5">Sin trucos aún.</p>
             )}
           </div>
         </>
