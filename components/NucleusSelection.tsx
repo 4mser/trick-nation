4f7079e1fe188643e13043fe126a927c9121e9ae
4mser <nicolas.moreno.sagan@gmail.com>
2024-06-12 18:05:52 -0400

    cunucleo

--- components/NucleusSelection.tsx
+++ components/NucleusSelection.tsx
@@ -12,33 +12,37 @@ type NucleusSelectionProps = {
 const nuclei = [
   {
     name: 'Enigma',
-    backendName:'Enigma',
+    backendName: 'Enigma',
     description: 'Rodeado de misterios y conocimientos ocultos, el Núcleo Enigma es un refugio para los curiosos del mundo esotérico y paranormal. Es ideal para aquellos que buscan descubrir los secretos más profundos de la existencia, explorando enigmas que desafían la lógica.',
     bgColor: 'bg-gradient-to-br from-indigo-600/20 to-transparent border border-indigo-400/40',
+    shadowColor: 'shadow-indigo-400/30',
     image: '/images/enigma.jpeg',
     profiles: 'Sabio, Visionario, Aventurero, Guerrero'
   },
   {
     name: 'Quantum',
-    backendName:'Quantum',
+    backendName: 'Quantum',
     description: 'Inspirado en la frontera de la ciencia y la tecnología, el Núcleo Quantum se dedica a la innovación y al descubrimiento científico. Es el hogar de pioneros comprometidos con los misterios de la realidad y el avance de la humanidad mediante la tecnología moderna.',
     bgColor: 'bg-gradient-to-br from-cyan-600/20 to-transparent border border-cyan-700',
+    shadowColor: 'shadow-cyan-700/30',
     image: '/images/quantum.jpeg',
     profiles: 'Creador, Guardián, Conector'
   },
   {
     name: 'Arbóreo',
-    backendName:'Arboreo',
+    backendName: 'Arboreo',
     description: 'Enraizada en el amor y el respeto por la naturaleza, el Núcleo Arbóreo valora la conservación del medio ambiente y la biodiversidad. Sus miembros se esfuerzan por vivir en armonía con el planeta, promoviendo un futuro sostenible para todas las formas de vida.',
     bgColor: 'bg-gradient-to-br from-lime-600/20 to-transparent border border-lime-700',
+    shadowColor: 'shadow-lime-700/30',
     image: '/images/arboreo.jpeg',
     profiles: 'Guardián, Aventurero, Sabio'
   },
   {
     name: 'Áureo',
-    backendName:'Aureo',
+    backendName: 'Aureo',
     description: 'Un santuario de la creatividad y la expresión, el Núcleo Áureo celebra las artes en todas sus manifestaciones. Desde la pintura hasta la música y el deporte, es un lugar para aquellos que sueñan con inspirar al mundo a través de su arte y creatividad única.',
     bgColor: 'bg-gradient-to-br from-yellow-600/20 to-transparent border border-yellow-700',
+    shadowColor: 'shadow-yellow-700/30',
     image: '/images/aureo.jpeg',
     profiles: 'Creador, Guerrero, Conector, Visionario'
   },
@@ -85,15 +89,20 @@ const NucleusSelection: React.FC<NucleusSelectionProps> = ({ onComplete }) => {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: index * 0.1, duration: 0.5, ease: 'easeInOut' }}
-                className={`relative ${nucleus.bgColor} text-white shadow-lg h-full cursor-pointer p-6 rounded-lg`}
+                className={`relative ${nucleus.bgColor} text-white shadow-lg h-full cursor-pointer p-6 rounded-lg transition-all duration-500 ${
+                  selectedNucleus === nucleus.backendName ? `${nucleus.shadowColor} shadow-xl` : 'shadow'
+                }`}
                 onClick={() => handleNucleusSelect(nucleus.backendName)}
               >
                 <div className="absolute top-4 left-4">
-                  <div
-                    className={`w-8 h-8 rounded-full border-[3px] ${
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.3 }}
+                    className={`w-8 h-8 rounded-full border-[3px] transition-all duration-500 ${
                       selectedNucleus === nucleus.backendName ? 'bg-yellow-500/80 border-yellow-500 shadow-xl shadow-yellow-500/70' : 'border-white bg-white/30'
                     }`}
-                  ></div>
+                  ></motion.div>
                 </div>
                 <img src={nucleus.image} alt={`${nucleus.name} icon`} className="w-full h-64 object-cover rounded-lg mb-4" />
                 <div className="flex flex-col space-y-2 flex-grow">
