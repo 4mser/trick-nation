548164b9ccf2b13b0ad3fb9da04f3d77a81f02b8
4mser <nicolas.moreno.sagan@gmail.com>
2024-07-04 19:38:54 -0400

    test viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <html lang="en">
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        </head>
         <body className="bg-neutral-950">
           <Header />
           <div>
